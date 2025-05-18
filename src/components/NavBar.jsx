f09cf789e1077a826bab31e71b2dd9b389f68758
aravindashokk <aravindkayal@gmail.com>
2025-05-18 11:53:12 -0500

    feat(ui): handleLogout with redirect to /login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
     const user = useSelector((store) => store.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await axios.post(BASE_URL + "/logout", {}, {
+                withCredentials: true,
+            });
+            dispatch(removeUser());
+            navigate('/login');
+        }
+        catch (err) {
+            console.error('Error :', err);
+        }
+    }
 
     return (
         <div className="navbar bg-base-300 shadow-sm">
@@ -32,7 +51,7 @@ const NavBar = () => {
                                 </Link>
                             </li>
                             <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
+                            <li><a onClick={handleLogout}>Logout</a></li>
                         </ul>
                     </div>
                 </div>)}
