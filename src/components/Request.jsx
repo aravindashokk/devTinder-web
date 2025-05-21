5ff6860ad708dbf20e1cf3cd14a6b1ba7fbaf8fc
aravindashokk <aravindkayal@gmail.com>
2025-05-20 22:09:22 -0500

    feat(ui): added connection request page to see all connection request

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -26,9 +26,37 @@ const Request = () => {
         fetchRequest();
     }, []);
 
+    if (!request) return;
+
+    if (request.length === 0) {
+        return (
+            <div className='flex justify-center my-10'>
+                <h1 className='text-bold text-2xl '>No Requests Found</h1>
+            </div>
+        )
+    }
+
+
     return (
-        <div>
-            <h1>Request</h1>
+        <div className='text-center my-10'>
+            <h1 className='text-bold text-white text-3xl '>Request</h1>
+            {request.map((req) => {
+                const { _id, firstName, lastName, photoUrl, age, gender, about } = req.fromUserId;
+                return (
+                    <div key={_id} className='flex justify-between items-center  m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto '>
+                        <div><img alt="photo" className="w-20 h-20 rounded-full" src={photoUrl} /></div>
+                        <div className='text-left mx-4'>
+                            <h2 className='font-bold text-xl'>{firstName + " " + lastName}</h2>
+                            {age && gender && (<p> {age + " " + gender}</p>)}
+                            <p>{about}</p>
+                        </div>
+                        <div>
+                            <button className="btn btn-primary mx-2">Reject</button>
+                            <button className="btn btn-secondary mx-2">Accept</button>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
